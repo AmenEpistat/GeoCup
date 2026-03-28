af03b3cb58f4bd55bae3b5a4fbd43c5a17ffbd71
luiza <luizaxakieva@gmail.com>
2026-03-29 01:26:23 +0300

    style: add common styles

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            'mapbox-gl': 'maplibre-gl',
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                    @use "@/styles/variables.scss" as *;
+                `,
+            },
+        },
+    },
+});
